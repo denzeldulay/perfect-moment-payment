79a020f4a1ef77f8a886d4c2e8965730280a63ad
denzeldulay <denzeldulay7@gmail.com>
2022-11-15 17:07:41 +0800

    test coomit - 2

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.post("/api/orders", async (req, res) => {
     const order = await paypal.createOrder();
     res.json(order);
   } catch (err) {
-    //res.status(500).send(err.message);
+    res.status(500).send(err.message);
   }
 });
 
