d3b23af5234c0ffe89d07d7022022a2a1920d689
denzeldulay <denzeldulay7@gmail.com>
2022-11-19 20:22:09 +0800

    added firebase

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import "dotenv/config"; // loads variables from .env file
 import express from "express";
 import * as paypal from "./paypal-api.js";
+import * as firebase_admin_app from "firebase-admin/app";
+import * as firebase_admin_fstr from "firebase-admin/firestore";
+import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
+
+firebase_admin_app.initializeApp({
+  credential: firebase_admin_app.cert(serviceAccount),
+});
 
 const app = express();
+const db = firebase_admin_fstr.getFirestore();
 
 app.use(express.static("public"));
 
@@ -14,6 +22,8 @@ app.post("/api/orders", async (req, res) => {
   try {
     const order = await paypal.createOrder();
     res.json(order);
+
+    await testDbWrite();
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -28,5 +38,9 @@ app.post("/api/orders/:orderID/capture", async (req, res) => {
     res.status(500).send(err.message);
   }
 });
+
+async function testDbWrite() {
+  await db.collection("test").doc().set({ test: "this is a test" });
+}
 
 app.listen(8888);
